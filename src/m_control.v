31f457cd71673f46d8c87b740913ae4934f2814f
Matheus Santos <matheusmat88@gmail.com>
2025-07-12 16:08:20 -0300

    m_controls

--- src/m_control.v
+++ src/m_control.v
@@ -1,3 +1,15 @@
+//FSM States
+`define IDLE        2'd0
+`define CONFIG_MODE 2'd1
+`define AUTO_MODE   2'd2
+`define MANUAL_MODE 2'd3
+
+//Channels
+`define CHANNEL1     2'd0
+`define CHANNEL2     2'd1
+`define CHANNEL3     2'd2
+`define CHANNEL4     2'd3
+
 module main_control(
     // Main_control inputs
     input         clk,
@@ -17,17 +29,6 @@ module main_control(
     output        en_mux,
     output [19:0] timer
 );
-    //FSM States
-    `define IDLE        2'd0
-    `define CONFIG_MODE 2'd1
-    `define AUTO_MODE   2'd2
-    `define MANUAL_MODE 2'd3
-
-    //Channels
-    `define CHANNEL1     2'd0
-    `define CHANNEL2     2'd1
-    `define CHANNEL3     2'd2
-    `define CHANNEL4     2'd3
 
     //Memory_mapped variables
     wire        fallback_enable;
@@ -162,31 +163,26 @@ module main_control(
         begin
             //if fallback_enable = 1, the function returns the highest priority when counters are same.
             if(fallback_enable) begin
-
                 // if priority channel counter 3 < priority channel counter 2, return priority channel 3, else return priority channel 2
-                partial_result[0] = (priority_decode(channel_priority[7:6],err_count) < priority_decode(channel_priority[5:4],err_count)) ? channel_priority[7:6]: channel_priority[5:4];
-                $display("teste: %d | chnel: ",priority_decode(channel_priority[7:6],err_count), channel_priority[7:6]);
                 // if priority channel counter 1 < priority channel counter 0, return priority channel 1, else return priority channel 0
+                partial_result[0] = (priority_decode(channel_priority[7:6],err_count) < priority_decode(channel_priority[5:4],err_count)) ? channel_priority[7:6]: channel_priority[5:4];
                 partial_result[1] = (priority_decode(channel_priority[3:2],err_count) < priority_decode(channel_priority[1:0],err_count)) ? channel_priority[3:2]: channel_priority[1:0];
-                
                 select_new_channel = (priority_decode(partial_result[0],err_count) < priority_decode(partial_result[1],err_count)) ? partial_result[0] : partial_result[1];
+                $display("teste: %d | chnel: ",priority_decode(channel_priority[7:6],err_count), channel_priority[7:6]);
             end
             //if fallback_enable = 0, the function stays the current channel when counters are same.
             else begin
-
                 // if priority channel counter 3 <= priority channel counter 2, return priority channel 3, else return priority channel 2
-                partial_result[0] = (priority_decode(channel_priority[7:6],err_count) <= priority_decode(channel_priority[5:4],err_count)) ? channel_priority[7:6]: channel_priority[5:4];
-                
                 // if priority channel counter 1 <= priority channel counter 0, return priority channel 1, else return priority channel 0
+                partial_result[0] = (priority_decode(channel_priority[7:6],err_count) <= priority_decode(channel_priority[5:4],err_count)) ? channel_priority[7:6]: channel_priority[5:4];
                 partial_result[1] = (priority_decode(channel_priority[3:2],err_count) <= priority_decode(channel_priority[1:0],err_count)) ? channel_priority[3:2]: channel_priority[1:0];
-            
                 select_new_channel = (priority_decode(partial_result[0],err_count) <= priority_decode(partial_result[1],err_count)) ? partial_result[0] : partial_result[1];
 
             end
         end
 
     endfunction
-
+    //This function returns the channel counter;
     function [7:0] priority_decode(
         input [1:0]  var_decode,
         input [31:0] err_count
